498e624327db8292e792bec4417e3b7ba9ad3edf
g.sarbaj@icloud.com <g.sarbaj@icloud.com>
2024-02-06 11:51:17 +0200

    antd added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,202 @@
-import React from 'react';
-import { Button, Input } from 'antd';
+'use client'
 
-const Home = () => (
-    <div className="App">
-        <Button type="primary">Button</Button>
-        <Input placeholder="Basic usage" />
-    </div>
-);
+import { useState } from 'react';
+import { Button, Input, message, Space, Modal, Col, DatePicker, Drawer, Form, Row, Select } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 
-export default Home;
+const { Option } = Select;
+
+export default function Home () {
+
+    const [open, setOpen] = useState(false);
+
+    const showDrawer = () => {
+        setOpen(true);
+    };
+
+    const onClose = () => {
+        setOpen(false);
+    };
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
+    const [messageApi, contextHolder] = message.useMessage();
+
+    const success = () => {
+        messageApi.open({
+            type: 'success',
+            content: 'This is a success message',
+        });
+    };
+
+    const error = () => {
+        messageApi.open({
+            type: 'error',
+            content: 'This is an error message',
+        });
+    };
+
+    const warning = () => {
+        messageApi.open({
+            type: 'warning',
+            content: 'This is a warning message',
+        });
+    };
+
+    return (
+        <>
+            {contextHolder}
+            <Space>
+                <Button onClick={success}>Success</Button>
+                <Button onClick={error}>Error</Button>
+                <Button onClick={warning}>Warning</Button>
+                <Button type="primary" onClick={showModal}>
+                    Open Modal
+                </Button>
+                <Button type="primary" onClick={showDrawer} icon={<PlusOutlined />}>
+                    New account
+                </Button>
+            </Space>
+
+
+
+                <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                </Modal>
+
+
+            <Drawer
+                title="Create a new account"
+                width={720}
+                onClose={onClose}
+                open={open}
+                styles={{
+                    body: {
+                        paddingBottom: 80,
+                    },
+                }}
+                extra={
+                    <Space>
+                        <Button onClick={onClose}>Cancel</Button>
+                        <Button onClick={onClose} type="primary">
+                            Submit
+                        </Button>
+                    </Space>
+                }
+            >
+                <Form layout="vertical" hideRequiredMark>
+                    <Row gutter={16}>
+                        <Col span={12}>
+                            <Form.Item
+                                name="name"
+                                label="Name"
+                                rules={[{ required: true, message: 'Please enter user name' }]}
+                            >
+                                <Input placeholder="Please enter user name" />
+                            </Form.Item>
+                        </Col>
+                        <Col span={12}>
+                            <Form.Item
+                                name="url"
+                                label="Url"
+                                rules={[{ required: true, message: 'Please enter url' }]}
+                            >
+                                <Input
+                                    style={{ width: '100%' }}
+                                    addonBefore="http://"
+                                    addonAfter=".com"
+                                    placeholder="Please enter url"
+                                />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row gutter={16}>
+                        <Col span={12}>
+                            <Form.Item
+                                name="owner"
+                                label="Owner"
+                                rules={[{ required: true, message: 'Please select an owner' }]}
+                            >
+                                <Select placeholder="Please select an owner">
+                                    <Option value="xiao">Xiaoxiao Fu</Option>
+                                    <Option value="mao">Maomao Zhou</Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                        <Col span={12}>
+                            <Form.Item
+                                name="type"
+                                label="Type"
+                                rules={[{ required: true, message: 'Please choose the type' }]}
+                            >
+                                <Select placeholder="Please choose the type">
+                                    <Option value="private">Private</Option>
+                                    <Option value="public">Public</Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row gutter={16}>
+                        <Col span={12}>
+                            <Form.Item
+                                name="approver"
+                                label="Approver"
+                                rules={[{ required: true, message: 'Please choose the approver' }]}
+                            >
+                                <Select placeholder="Please choose the approver">
+                                    <Option value="jack">Jack Ma</Option>
+                                    <Option value="tom">Tom Liu</Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                        <Col span={12}>
+                            <Form.Item
+                                name="dateTime"
+                                label="DateTime"
+                                rules={[{ required: true, message: 'Please choose the dateTime' }]}
+                            >
+                                <DatePicker.RangePicker
+                                    style={{ width: '100%' }}
+                                    getPopupContainer={(trigger) => trigger.parentElement!}
+                                />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row gutter={16}>
+                        <Col span={24}>
+                            <Form.Item
+                                name="description"
+                                label="Description"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'please enter url description',
+                                    },
+                                ]}
+                            >
+                                <Input.TextArea rows={4} placeholder="please enter url description" />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                </Form>
+            </Drawer>
+
+
+        </>
+    );
+};
+
---
+++ theme/themeConfig.ts
@@ -0,0 +1,12 @@
+import type { ThemeConfig } from 'antd';
+
+const theme: ThemeConfig = {
+    token: {
+        fontSize: 14,
+        "colorPrimary": "#5e2ba6",
+        "colorInfo": "#5e2ba6",
+        "borderRadius": 6
+    },
+};
+
+export default theme;
